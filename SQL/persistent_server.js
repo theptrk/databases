e9fb67c98cf12f5dff33da4e08a242884afc3d1f
Hack Reactor Students <patrick.tran06@gmail.com>
2014-05-09 23:58:34 -0700

    Isolate database code to file

--- SQL/persistent_server.js
+++ SQL/persistent_server.js
@@ -1,4 +1,7 @@
+var http = require('http');
+var fs = require('fs');
 var mysql = require('mysql');
+var serve = require('../chatterbox-server-1/server/server.js')
 /* If the node mysql module is not found on your system, you may
  * need to do an "sudo npm install -g mysql". */
 
@@ -6,12 +9,19 @@ var mysql = require('mysql');
  * database: "chat" specifies that we're using the database called
  * "chat", which we created by running schema.sql.*/
 var dbConnection = mysql.createConnection({
-  user: "",
+  user: "root",
   password: "",
   database: "chat"
 });
 
 dbConnection.connect();
+
+dbConnection.query("SELECT * from messages", function(err, rows, fields){
+  if (err) {
+    console.log(err)
+  };
+  console.log(rows)
+})
 /* Now you can make queries to the Mysql database using the
  * dbConnection.query() method.
  * See https://github.com/felixge/node-mysql for more details about
