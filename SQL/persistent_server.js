95b5f0cd4f234d8cd2ea4c5a2614178931cd19f3
Hack Reactor Students <patrick.tran06@gmail.com>
2014-05-10 11:54:52 -0700

    Stock SQL information

--- SQL/persistent_server.js
+++ SQL/persistent_server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var mysql = require('mysql');
-var serve = require('../chatterbox-server-1/server/server.js')
+//var serve = require('../chatterbox-server-1/server/server.js');
+//
 /* If the node mysql module is not found on your system, you may
  * need to do an "sudo npm install -g mysql". */
 
@@ -16,16 +17,12 @@ var dbConnection = mysql.createConnection({
 
 dbConnection.connect();
 
-dbConnection.query("SELECT * from messages", function(err, rows, fields){
-  if (err) {
-    console.log(err)
-  };
-  console.log(rows)
-})
-/* Now you can make queries to the Mysql database using the
- * dbConnection.query() method.
- * See https://github.com/felixge/node-mysql for more details about
- * using this module.*/
+exports.dbConnection = dbConnection;
 
-/* You already know how to create an http server from the previous
- * assignment; you can re-use most of that code here. */
+// dbConnection.query("SELECT * from messages", function(err, rows, fields){
+//   if (err) {
+//     console.log(err);
+//   };
+//   console.log('all messages');
+//   console.log(rows);
+// });
